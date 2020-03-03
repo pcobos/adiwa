03b4099c52be2cf55b810c44b26feab92449568d
Jose Carlos Juarez <josecarlos.juarez@gmail.com>
2020-03-03 14:39:30 -0600

    crreated first routes without sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :ceremonies, only: [:index, :show, :new, :create, :destroy] do
+    resources :bookings, only: [:new, :create]
+  end
+
+  resources :users, only: [:index, :show, :create, :new] do
+    resources :bookings, only: [:index]
+    resources :ceremonies, only: [:index]
+  end
+
+
 end
