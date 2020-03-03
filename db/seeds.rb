44c5f2d3cf65739cad58399d85ea587d5540fd5e
mauricio davila <mauricioadb@gmail.com>
2020-03-03 15:56:48 -0600

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+
+User.destroy_all
+Ceremony.destroy_all
+
+user = User.create!(
+  email: "user@user.com",
+  password: 123456
+)
+
+medicines = ['Peyote', 'San Pedro', 'Mushrooms', 'Kambo', 'Bufo Alvarius', 'Yopo', 'Ayahuasca' ]
+
+20.times do |n|
+  ceremony = Ceremony.new(
+    name: "#{medicines.sample} Ceremony",
+    location: Faker::Address.full_address,
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit " * 5,
+    total_price: rand(1000..3000),
+    duration: "#{rand(1..10)} days",
+    user: user
+    )
+
+  ceremony.save!
+end
