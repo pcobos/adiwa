37db3106f16273aef1f480816c6079f12cc2a12a
Max Dupont <maxime.maletdupont@gmail.com>
2020-03-03 18:32:49 -0500

    done

--- app/controllers/ceremonies_controller.rb
+++ app/controllers/ceremonies_controller.rb
@@ -5,12 +5,28 @@ class CeremoniesController < ApplicationController
     end
 
   def show
+    @ceremony = Ceremony.find(params[:id])
   end
 
   def new
+    @ceremony = Ceremony.new
+    @user = current_user
   end
 
   def create
+    @ceremony = Ceremony.new(ceremony_params)
+    @user = User.new(params[:user_id])
+    @ceremony.user = @user
+    if @ceremony.save
+      redirect_to ceremonies_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ceremony_params
+    params.require(:ceremony).permit(:name, :location, :duration, :total_price, :description)
   end
 end
-
