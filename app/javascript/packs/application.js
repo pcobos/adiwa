0b3b0b7197fde3a78232c0bd1a95a7b0162ad2cb
Max Dupont <maxime.maletdupont@gmail.com>
2020-03-05 18:20:16 -0500

    done

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,9 @@
 import "bootstrap";
-
-import { initUpdateNavbarOnScroll } from '../components/navbar';
-initUpdateNavbarOnScroll();
-
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { initMapbox } from '../plugins/init_mapbox';
+import { gallery } from '../components/gallery';
 
+initUpdateNavbarOnScroll();
 initMapbox();
