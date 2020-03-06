dbfc1101ce787227b8c8eecbc089e3abebf65dba
Pedro Cobos <pedro.cobos@live.com>
2020-03-06 17:22:16 -0600

    navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,6 @@ module ApplicationHelper
     current_page?(new_user_session_path) ||
     current_page?(new_user_registration_path) ||
     current_page?(new_ceremony_path) ||
-    current_page?(new_ceremony_session_path) ||
     current_page?(new_user_session_url)
   end
 end
